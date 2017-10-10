3281aaa8a6919f05aee2f7d712d353df2ce77347
Niick555 <logusbol@hotmail.com>
2017-10-10 20:55:48 -0300

    Funcionando para dados de entrada de 16 bytes, e hash não recebe do scanf

--- cryptodevice.c
+++ cryptodevice.c
@@ -6,26 +6,41 @@
 #include <asm/uaccess.h>
 #include <linux/crypto.h>
 #include <linux/scatterlist.h>
+#include <crypto/hash.h>
 #define  DEVICE_NAME "cryptodevice"
 #define  CLASS_NAME  "cryptodevice"
 
 MODULE_LICENSE("GPL");
 
+#define SIZE_OF_DATA 16
+
 static int    majorNumber;
-static char   data[32];
-static short  size_of_data;
+static char   data[SIZE_OF_DATA];
+static short  size_of_data = 0;
 static struct class*  cryptoClass  = NULL;
 static struct device* cryptoDevice = NULL;
-static char   operation;
+static char   operation = 0;
 static char   *key;
+static u8     criptografado[SIZE_OF_DATA], descriptografado[SIZE_OF_DATA];
+static u8     hashOutput[SIZE_OF_DATA];
 
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg);
+static int calc_hash(struct crypto_shash *alg, const unsigned char *data, unsigned int datalen, unsigned char *out);
 
 module_param(key, charp, 0000);
 
 static struct file_operations fops =
 {
+   .read = dev_read,
    .write = dev_write,
+};
+
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
 };
 
 static int __init cryptodevice_init(void){
@@ -66,13 +81,31 @@ static void __exit cryptodevice_exit(void){
    printk(KERN_INFO "cryptodevice: finalizado com sucesso!\n");
 }
 
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+   char buf[256];
+
+   buf[0] = operation;
+
+   if(operation == 'c') {
+      sprintf(&(buf[1]), "%s", criptografado);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
+   else if(operation == 'd') {
+      sprintf(&(buf[1]), "%s", descriptografado);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
+   else if(operation == 'h') {
+      sprintf(&(buf[1]), "%s", hashOutput);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
+
+   return 0;
+}
+
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
 	struct crypto_cipher *cryptoCipher = NULL;
-   struct crypto_hash *cryptoHash;
-   struct hash_desc cryptoHashDesc;
-   struct scatterlist sg;
-	u8 criptografado[16], descriptografado[16];
-   unsigned char hashOutput[32];
+   struct crypto_shash *cryptoSHash = NULL;
+   int i;
 
 	cryptoCipher = crypto_alloc_cipher("aes", 0, 0);
 
@@ -81,22 +114,12 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 		return PTR_ERR(crypto_cipher_tfm(cryptoCipher));
 	}
 
-   cryptoHash = crypto_alloc_hash("sha1", 0, 0);
-
-   if (IS_ERR(crypto_hash_tfm(cryptoHash))) {
-      pr_info("cryptodevice: não foi possível alocar o handle para o hash\n");
-      return PTR_ERR(crypto_hash_tfm(cryptoHash));
-   }
-
-   cryptoHashDesc.tfm = cryptoHash;
-   cryptoHashDesc.flags = 0;
+   cryptoSHash = crypto_alloc_shash("sha1", 0, 0);
 
    if (crypto_cipher_setkey(cryptoCipher, key, 32 * sizeof(u8)) != 0) {
       pr_info("cryptodevice: não foi possível definir a key\n");
       return 1;
    }
-
-   //TEM QUE VERIFICAR SE A PESSOA EH SACANA!!!!!!!!!! URGENTE
    
    sprintf(data, "%s", &(buffer[2]));
    size_of_data = strlen(data);
@@ -111,7 +134,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
    	   crypto_cipher_encrypt_one(cryptoCipher, criptografado, data);
    	   printk(KERN_INFO "cryptodevice: a operacao recebida é %c, resultado: ", operation);
 
-         for(int i = 0; i < sizeof(criptografado); i++) {
+         for(i = 0; i < sizeof(criptografado); i++) {
             printk(KERN_CONT "%02x", criptografado[i]);
          }
 
@@ -121,7 +144,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
       int j = 0;
 
       printk(KERN_INFO "cryptodevice: criptografado[]: ");
-      for(int i = 0; i < 32; i++) {
+      for(i = 0; i < SIZE_OF_DATA; i++) {
          if(i % 2 == 0) {
             char aux[2];
             unsigned long res = 0;
@@ -141,21 +164,43 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
    	printk(KERN_INFO "cryptodevice: a operacao recebida é %c, resultado: %s\n", operation, descriptografado);
    }
    else if(operation == 'h') {
-      crypto_hash_init(&cryptoHashDesc);
-      sg_init_one(&sg, data, 32);
-      crypto_hash_update(&cryptoHashDesc, &sg, 32);
-      crypto_hash_final(&cryptoHashDesc, hashOutput);
+      char teste[32] = "AAAABBBBAAAABBBBAAAABBBBAAAABBBD";
+      calc_hash(cryptoSHash, teste, 32, hashOutput);
 
       printk(KERN_INFO "cryptodevice: a operacao recebida é %c, resultado: ", operation);
 
-      for(int i = 0; i < sizeof(hashOutput); i++) {
+      for(i = 0; i < sizeof(hashOutput); i++) {
          printk(KERN_CONT "%02x", hashOutput[i]);
       }
    }
    
    crypto_free_cipher(cryptoCipher);
-   crypto_free_hash(cryptoHash);
+   crypto_free_shash(cryptoSHash);
    return len;
+}
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg) {
+    struct sdesc *sdesc;
+    int size;
+
+    size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+    sdesc = kmalloc(size, GFP_KERNEL);
+    
+    sdesc->shash.tfm = alg;
+    sdesc->shash.flags = 0x0;
+
+    return sdesc;
+}
+
+static int calc_hash(struct crypto_shash *alg, const unsigned char *data, unsigned int datalen, unsigned char *out) {
+    struct sdesc *sdesc;
+    int ret;
+
+    sdesc = init_sdesc(alg);
+
+    ret = crypto_shash_digest(&sdesc->shash, data, datalen, out);
+    kfree(sdesc);
+    return ret;
 }
 
 module_init(cryptodevice_init);
